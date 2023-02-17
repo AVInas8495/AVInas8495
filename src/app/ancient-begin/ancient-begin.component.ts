6f9260dcc626a74dcd4eb73d4b57d81d6c4b733a
yatish <er.yatish@gmail.com>
2023-02-17 22:38:07 +0530

    first-screen-complete-function

--- src/app/ancient-begin/ancient-begin.component.ts
+++ src/app/ancient-begin/ancient-begin.component.ts
@@ -14,9 +14,18 @@ export class AncientBeginComponent {
   visible:boolean=false;
   visible1:boolean=false;
   visible2:boolean=false;
-  visible3:boolean=false;
-  visible4:boolean=false;
-  visible5:boolean=false;
+  // section 2 variables
+  main_options_left:boolean=false;
+  main_options_right:boolean=false;
+  revolt: boolean=  false;
+  indian_council:boolean=  false;
+  face_ww1: boolean = false;
+  gov_act: boolean = false;
+  submenu_revolt: boolean = false;
+  submenu_face_ww1: boolean = false;
+  submenu_gov_act: boolean = false;
+  submenu_indian_council: boolean = false;
+  // section 2 variables end
 
   public showTab(tab: string) {
     this.tab = tab;
@@ -27,8 +36,10 @@ export class AncientBeginComponent {
   }
 
   showContentpart(){
-    this.visible2=!this.visible2;
-    if(this.visible2)
+    this.submenu_revolt=!this.submenu_revolt;
+    this.indian_council = !this.indian_council;
+    this.main_options_right = !this.main_options_right
+    if(this.submenu_revolt)
       this.showTabpart('tab12')
     else
       this.showTabpart('tab11')
@@ -55,8 +66,10 @@ showContent(){
     }
 
     showContentpart1(){
-      this.visible3=!this.visible3;
-      if(this.visible3)
+      this.submenu_indian_council = !this.submenu_indian_council;
+      this.revolt = !this.revolt;
+      this.main_options_right = !this.main_options_right;
+      if(this.submenu_indian_council)
       this.showTabpart('tab16')
     else
       this.showTabpart('tab11')
@@ -64,19 +77,25 @@ showContent(){
 
 
       showContentpart2(){
-        this.visible4=!this.visible4;
-        if(this.visible4)
-        this.showTabpart('tab21')
-      else
-        this.showTabpart('tab11')
+        this.revolt = !this.revolt;
+        this.indian_council = !this.indian_council;
+        this.gov_act = !this.gov_act;
+        this.submenu_face_ww1 = !this.submenu_face_ww1;
+        if(this.submenu_face_ww1)
+          this.showTabpart('tab21')
+        else
+          this.showTabpart('tab11')
         }
 
         showContentpart3(){
-          this.visible5=!this.visible5;
-          if(this.visible5)
-          this.showTabpart('tab23')
-        else
-          this.showTabpart('tab11')
+          this.face_ww1 = !this.face_ww1;
+          this.revolt = !this.revolt;
+          this.indian_council = !this.indian_council;
+          this.submenu_gov_act = !this.submenu_gov_act;
+          if(this.submenu_gov_act)
+            this.showTabpart('tab23')
+          else
+            this.showTabpart('tab11')
           }
 
   ngOnInit(): void {
