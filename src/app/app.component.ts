759070863c09daec865c09aa797a3704a530cf58
Ronny Baidya <rbaidya@bluemeteor.com>
2023-03-26 23:26:32 +0530

    Disabled right click and zoom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { DisableRightClickService } from './disable-right-click.service';
 
 @Component({
   selector: 'app-root',
@@ -10,17 +11,18 @@ export class AppComponent {
 
   public langen: string;
   public langhn: string;
-  
-  constructor (public translate:TranslateService){
+
+  constructor(public translate: TranslateService, private rightClickDisable: DisableRightClickService) {
 
     this.langen = "English";
     this.langhn = "हिंदी";
-
-    translate.addLangs(['en','hn']);
+    translate.addLangs(['en', 'hn']);
     translate.setDefaultLang('en');
     translate.use('hn')
     const browserLang = translate.getBrowserLang();
-    translate.use(browserLang?.match(/en|hn/) ? browserLang : 'en'); 
-
+    translate.use(browserLang?.match(/en|hn/) ? browserLang : 'en');
+  }
+  ngOnInit() {
+    this.rightClickDisable.disableRightClick();
   }
 }
